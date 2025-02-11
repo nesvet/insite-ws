bc63c11fa697c5a823582be2014399504be58e9f
Eugene Nesvetaev <dev@nesvet.dev>
2025-02-11 09:59:32 +0400

    refactor: No more local insite-common

--- .conveyer.js
+++ .conveyer.js
@@ -17,7 +17,6 @@ new Conveyer([
 		entryPoints: [ "src/server/index.ts" ],
 		outfile: path.resolve(distDir, "server/index.js"),
 		external: [ true, "insite-*" ],
-		local: "insite-common",
 		platform: "node",
 		target: "node20",
 		...common
